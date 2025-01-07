7d383a5c17ff482b6b47dc1a17c0de35bd4023f8
nublin <polina.ivoylova1@gmail.com>
2025-01-07 21:21:12 +0400

    improvement of view functions

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from ice_cream.models import IceCream
 
+
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
+
     ice_cream = get_object_or_404(
-        IceCream.objects.values(
-            'title', 'description'
+        IceCream.objects.filter(
+            is_published=True,
+            category__is_published=True
         ),
         pk=pk
     )
@@ -17,5 +20,13 @@ def ice_cream_detail(request, pk):
 
 def ice_cream_list(request):
     template = 'ice_cream/list.html'
-    context = {}
+
+    ice_cream_list = IceCream.objects.select_related('category').filter(
+       is_published=True,
+       category__is_published=True
+    ).order_by('category')
+
+    context = {
+        'ice_cream_list': ice_cream_list
+    }
     return render(request, template, context)
